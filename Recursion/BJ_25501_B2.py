92a094ac3871c9468949a7804c937bb5cfb20490
subillie <subinlee@c1r7s7.42seoul.kr>
2022-12-19 05:26:32 +0900

    재귀의 귀재

---
+++ Recursion/BJ_25501_B2.py
@@ -0,0 +1,16 @@
+def recursion(s, l, r, count):
+    if l >= r:
+        return (1, count)
+    elif s[l] != s[r]:
+        return (0, count)
+    else:
+        return recursion(s, l + 1, r - 1, count + 1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s) - 1, 1)
+
+num = int(input())
+for i in range (num):
+    arr = input()
+    value, count = isPalindrome(arr)
+    print(f"{value} {count}")
