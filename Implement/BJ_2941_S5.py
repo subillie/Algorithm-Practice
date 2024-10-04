11220ebf8b53cbbc02a1fbc30ad6d7315758d387
Subin Lee <ddubi701@gmail.com>
2024-10-04 11:41:03 +0900

    reset

--- Implement/BJ_2941_S5.py
+++
@@ -1,6 +0,0 @@
-alpha = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-word = input()
-
-for i in alpha :
-    word = word.replace(i, '*')
-print(len(word))
--- Math/BJ_2745_B2.py
+++
@@ -1,3 +0,0 @@
-a, b = map(str, input().split())
-
-print(int(a, int(b)))
