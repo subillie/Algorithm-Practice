6eadada6391b9f25cf8f697c1308c9d134367c3b
subillie <ddubi701@gmail.com>
2025-07-16 17:51:32 +0900

    큐

---
+++ BJ Silver/s4_10845.py
@@ -0,0 +1,25 @@
+# 10845 큐
+from collections import deque
+import sys
+INPUT = sys.stdin.readline
+
+N = int(INPUT())
+queue = deque()
+for n in range(N):
+    cmd = INPUT().rstrip()
+    if cmd == "pop":
+        try:
+            print(queue.popleft())
+        except:
+            print(-1)
+    elif cmd == "size":
+        print(len(queue))
+    elif cmd == "empty":
+        print(0) if queue else print(1)
+    elif cmd == "front":
+        print(queue[0]) if queue else print(-1)
+    elif cmd == "back":
+        print(queue[-1]) if queue else print(-1)
+    else:  # cmd == "push"
+        param = cmd.split()[1]
+        queue.append(param)
