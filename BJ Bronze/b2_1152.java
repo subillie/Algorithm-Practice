1e5a3cca3b816f3145cc360cbf00cae1be7b58c4
subillie <ddubi701@gmail.com>
2025-07-17 13:22:22 +0900

    단어의 개수

---
+++ BJ Bronze/b2_1152.java
@@ -0,0 +1,15 @@
+// 1152 단어의 개수
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class b2_1152 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+        System.out.println(st.countTokens());
+    }
+}
