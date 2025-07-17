574c4a0e1670faa43ef3a9c2812e7cad688037e7
subillie <ddubi701@gmail.com>
2025-07-17 09:46:46 +0900

    1대1 가위바위보

---
+++ SWEA D1/d1_1936.java
@@ -0,0 +1,18 @@
+// 1936 1대1 가위바위보
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        if ((A == 1 && B == 2) || (A == 2 && B == 3) || (A == 3 && B == 1)){
+            System.out.println("B");
+        } else {
+            System.out.println("A");
+        }
+        
+        sc.close();
+    }
+}
