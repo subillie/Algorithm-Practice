f066ffc3150fa2592690d641aaf4a4529b4c41f6
subillie <ddubi701@gmail.com>
2025-07-21 11:35:44 +0900

    색종이

--- BJ Bronze/b1_10163.py
+++ BJ Bronze/b1_10163.py
@@ -8,8 +8,8 @@ coordinate_system = [[0] * 1001 for _ in range(1001)]
 # 색종이 쌓기
 for n in range(1, N + 1):
     x, y, width, height = map(int, INPUT().split())
-    for x_pos in range(x, x + width):
-        coordinate_system[x_pos][y:(y + height)] = [n] * height
+    for y_pos in range(y, y + height):
+        coordinate_system[y_pos][x:(x + width)] = [n] * width
 
 # 면적 구하기
 for n in range(1, N + 1):
