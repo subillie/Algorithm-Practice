3d1dcbebba647c105414efddba1b9ead5c95130b
subillie <ddubi701@gmail.com>
2025-07-24 08:57:54 +0900

    현주의 상자 바꾸기

---
+++ SWEA D3/d3_5789.py
@@ -0,0 +1,10 @@
+# 5789 현주의 상자 바꾸기
+T = int(input())
+for t in range(1, T + 1):
+    N, Q = map(int, input().split())
+    boxes = [0] * N
+    for i in range(1, Q + 1):
+        L, R = map(int, input().split())
+        for j in range(L - 1, R):
+            boxes[j] = i
+    print(f"#{t}", *boxes)
