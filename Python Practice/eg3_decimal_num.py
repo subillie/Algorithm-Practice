84ad1900c2d6d9e12ce23d8ef82da6e6c021a344
subillie <ddubi701@gmail.com>
2025-07-31 10:23:11 +0900

    올림, 내림, 반올림

--- Python Practice/eg3_decimal_num.py
+++ Python Practice/eg3_decimal_num.py
@@ -1,4 +1,4 @@
-# 소수점 조정
+# 올림, 내림, 반올림
 import math
 
 num = 1.23456
@@ -16,8 +16,8 @@ print("round", round(num - 1, 3))
 print("\n[ROUND 자릿수 지정 X]")
 print("round", round(num))  # 정수 부분이 홀수면 올림
 print("round", round(num - 1))  # 정수 부분이 짝수면 내림
-print("floor", math.floor(num + 0.5))  # 정수 부분 신경 안 쓰고 반올림하기
 
 print("\n[CEIL, FLOOR]")
 print("ceil", math.ceil(num))  # 올림
 print("floor", math.floor(num))  # 내림
+print("floor", math.floor(num + 0.5))  # 반올림
