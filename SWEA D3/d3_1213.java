4a6f3d3f00434540b0fb97e25ccc5412f5e4c4cf
subillie <ddubi701@gmail.com>
2025-08-06 15:17:39 +0900

    String

--- SWEA D3/d3_1213.java
+++ SWEA D3/d3_1213.java
@@ -13,18 +13,27 @@ public class d3_1213 {
 			String str = br.readLine().trim();
 			
 			int count = 0;
-			for (int i = 0; i < str.length(); i++) {
-				if (str.charAt(i) == target.charAt(0)) {
-					for (int j = 1; j < target.length(); j++) {
-						if (i + j >= str.length() || str.charAt(i + j) != target.charAt(j)) {
-							break;
-						}
-						if (j == target.length() - 1) {
-							count++;
-							i += j;
-						}
-					}
-				}
+            // 방법1 : 이중 for문 돌기
+			// for (int i = 0; i < str.length(); i++) {
+			// 	if (str.charAt(i) == target.charAt(0)) {
+			// 		for (int j = 1; j < target.length(); j++) {
+			// 			if (i + j >= str.length() || str.charAt(i + j) != target.charAt(j)) {
+			// 				break;
+			// 			}
+			// 			if (j == target.length() - 1) {
+			// 				count++;
+			// 				i += j;
+			// 			}
+			// 		}
+			// 	}
+			// }
+            // 방법2 : String 메서드 indexOf 사용하기
+			int fromIndex = 0;
+			while (true) {
+				fromIndex = str.indexOf(target, fromIndex);
+				if (fromIndex == -1) break;
+				count++;
+				fromIndex += target.length();
 			}
 			System.out.printf("#%d %d\n", T, count);
 		}
