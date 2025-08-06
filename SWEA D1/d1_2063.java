18f29fac08f8963fd854e503702d92c174a85aaa
subillie <ddubi701@gmail.com>
2025-08-06 15:11:31 +0900

    중간값 찾기

--- SWEA D1/d1_2063.java
+++ SWEA D1/d1_2063.java
@@ -1,19 +1,19 @@
 // 2063 중간값 찾기
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
 
-class Solution {
+public class d1_2063 {
 	public static void main(String args[]) throws Exception {
 		Scanner sc = new Scanner(System.in);
         
 		int n = sc.nextInt();
-    int[] scores = new int[n];
-    for (int i = 0; i < n; i++) {
-        scores[i] = sc.nextInt();
-    }
-    Arrays.sort(scores);
-    System.out.println(scores[n / 2]);
-    
-    sc.close();
+        int[] scores = new int[n];
+        for (int i = 0; i < n; i++) {
+            scores[i] = sc.nextInt();
+        }
+        Arrays.sort(scores);
+        System.out.println(scores[n / 2]);
+        
+        sc.close();
 	}
 }
