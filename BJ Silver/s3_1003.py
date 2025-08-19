5263e8480145a662d0e0efc6ba14116da61fc504
subillie <ddubi701@gmail.com>
2025-08-19 13:56:50 +0900

    피보나치 함수

---
+++ BJ Silver/s3_1003.py
@@ -0,0 +1,11 @@
+# 1003 피보나치 함수
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    zero, one = 1, 0
+    for _ in range(N):
+        zero, one = one, zero + one
+    print(zero, one)
