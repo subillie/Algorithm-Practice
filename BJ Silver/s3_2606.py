e2023e01c9ac00e732862c5716d2c7b12916ec55
Subin Lee <ddubi701@gmail.com>
2025-08-20 17:28:52 +0900

    바이러스

--- BJ Silver/s3_2606.py
+++ BJ Silver/s3_2606.py
@@ -1,27 +1,43 @@
 # 2606 바이러스
 from collections import deque
 import sys
-INPUT = sys.stdin.readline
+input = sys.stdin.readline
 
-node_num = int(INPUT().rstrip())
-edge_num = int(INPUT().rstrip())
-graph = [[] for _ in range(node_num + 1)]
-for _ in range(edge_num):
-    node_1, node_2 = map(int, INPUT().split())
-    graph[node_1].append(node_2)
-    graph[node_2].append(node_1)
+def bfs(n: int) -> None:
+  global count
+  queue = deque()
+  queue.append(n)
+  visited[1] = True
 
-queue = deque()
-visited = [False for _ in range(node_num + 1)]
+  while(queue):
+      node = queue.popleft()
+      for next_node in graph[node]:
+          if not visited[next_node]:
+              queue.append(next_node)
+              visited[next_node] = True
+              count += 1
 
-queue.append(1)
-visited[1] = True
-count = 0
-while(queue):
-    node = queue.popleft()
-    for next_node in graph[node]:
-        if visited[next_node] == False:
-            queue.append(next_node)
-            visited[next_node] = True
-            count += 1
-print(count)
+def dfs(n: int) -> None:
+  global count
+  visited[n] = True
+  for next_node in graph[n]:
+      if not visited[next_node]:
+          dfs(next_node)
+          count += 1
+
+if __name__ == "__main__":
+  node_num = int(input())
+  edge_num = int(input())
+  graph = [[] for _ in range(node_num + 1)]
+  for _ in range(edge_num):
+      node1, node2 = map(int, input().split())
+      graph[node1].append(node2)
+      graph[node2].append(node1)
+
+  count = 0
+  visited = [False for _ in range(node_num + 1)]
+
+  # bfs(1)
+  dfs(1)
+
+  print(count)
