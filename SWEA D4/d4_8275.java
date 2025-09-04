19379cc5ba948e5d59587527beb3af7dbe40655e
subillie <ddubi701@gmail.com>
2025-09-04 17:53:30 +0900

    햄스터

---
+++ SWEA D4/d4_8275.java
@@ -0,0 +1,17 @@
+// 8275 햄스터
+import java.io.*;
+import java.util.*;
+
+public class d4_8275 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            sb.append("#").append(t).append(" ").append(result).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
