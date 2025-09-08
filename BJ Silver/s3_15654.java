9304176d368c37475ab0df24f349ef08bb680665
subillie <ddubi701@gmail.com>
2025-09-08 11:01:51 +0900

    N과 M (5)

---
+++ BJ Silver/s3_15654.java
@@ -0,0 +1,54 @@
+// 15654 N과 M (5)
+import java.io.*;
+import java.util.*;
+
+public class s3_15654 {
+    private static int[] inputs, arr;
+    private static boolean[] visited;
+    private static StringBuilder sb = new StringBuilder();
+	
+	private static void backtrack(int N, int M, int depth) {
+        // Base case
+		if (depth == M) {
+            for (int num: arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+			return;
+		}
+
+        // Recursive case
+		for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = inputs[i];
+                backtrack(N, M, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Input
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        inputs = new int[N];
+        for (int i = 0; i < N; i++) {
+            inputs[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(inputs);
+        
+        // Logic
+        arr = new int[M];
+        visited = new boolean[N];
+		backtrack(N, M, 0);
+		
+        // Output
+		System.out.println(sb.toString());
+	}
+}
