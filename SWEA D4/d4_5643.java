bc0c95294a5b3733876c2dae6857fca0813fd617
subillie <ddubi701@gmail.com>
2025-09-11 14:49:32 +0900

    키 순서

--- SWEA D4/d4_5643.java
+++ SWEA D4/d4_5643.java
@@ -3,19 +3,16 @@ import java.io.*;
 import java.util.*;
 
 public class d4_5643 {
-    private static int N, count;
-    private static boolean[] visited;
+    private static int N;
+    private static BitSet[] graph;
 
-    private static void dfs(BitSet[] graph, int cur) {
-        visited[cur] = true;
-        
-        int next = graph[cur].nextSetBit(0);
-        while (next >= 0) {
-            if (!visited[next]) {
-                count++;
-                dfs(graph, next);
+    private static void floydWarshall() {
+        for (int j = 1; j <= N; j++) {     // j: 중간 경유지
+            for (int i = 1; i <= N; i++) { // i: 출발지
+                if (graph[i].get(j)) {     // i -> j 경로가 존재한다면
+                    graph[i].or(graph[j]); // i는 j의 경로 사용 가능하므로 합집합 연산 수행
+                }
             }
-            next = graph[cur].nextSetBit(next + 1);
         }
     }
 
@@ -29,34 +26,32 @@ public class d4_5643 {
             N = Integer.parseInt(br.readLine());
             int M = Integer.parseInt(br.readLine());
             
-            BitSet[] graph = new BitSet[N + 1];
-            BitSet[] revGraph = new BitSet[N + 1];
+            graph = new BitSet[N + 1]; // BitSet을 인접 행렬처럼 사용
             for (int i = 1; i <= N; i++) {
                 graph[i] = new BitSet(N + 1);
-                revGraph[i] = new BitSet(N + 1);
             }
 
             for (int i = 0; i < M; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-
-                graph[a].set(b);    // a -> b (a는 b보다 작다)
-                revGraph[b].set(a); // b -> a (b는 a보다 크다)
+                graph[a].set(b); // a -> b
             }
 
             // Logic
+            floydWarshall(); // 모든 경로 정보 갱신
+            
             int answer = 0;
-            visited = new boolean[N + 1];
             for (int i = 1; i <= N; i++) {
-                count = 0;
-
-                Arrays.fill(visited, false);
-                dfs(graph, i); // 나보다 키 큰 사람 수 세기
-                Arrays.fill(visited, false);
-                dfs(revGraph, i); // 나보다 키 작은 사람 수 세기
-
-                if (count == N - 1) { // 카운트가 나 제외 N-1이면 정답
+                int count = 0;
+                for (int j = 1; j <= N; j++) {
+                    // i가 j보다 작거나(i->j), i가 j보다 크면(j->i) 관계를 아는 것
+                    if (i != j && (graph[i].get(j) || graph[j].get(i))) {
+                        count++;
+                    }
+                }
+                // 나를 제외한 모든 학생(N-1)과의 관계를 알고 있으면 순서가 확정되므로 정답 카운트
+                if (count == N - 1) {
                     answer++;
                 }
             }
